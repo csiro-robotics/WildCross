692aeaf2f11070447de5cd21106576e7d625c610
David Hall <david.hall@csiro.au>
2025-12-11 14:54:24 +1100

    added missing surface normal load example

--- example_loaders.py
+++ example_loaders.py
@@ -20,7 +20,7 @@ def load_depth_image(depth_path: str) -> npt.NDArray[np.uint16]:
     '''
     Loads a given depth image from the disk
     Inputs:
-    - depth_path: String containing path the the depth file on disk
+    - depth_path: String containing path to the depth file on disk
     Outputs:
     - depth_mm: Numpy array of dimension [H x W], containing the depth associated with each pixel in mm
     '''
@@ -31,9 +31,25 @@ def load_rgb_image(image_path: str) -> npt.NDArray[np.uint8]:
     '''
     Loads a given RGB image from the disk
     Inputs:
-    - image_path: String containing path the the image file on disk
+    - image_path: String containing path to the image file on disk
     Outputs:
     - image_array: 3 channel numpy array of dimension [H x W x 3], representing the RGB image.
     '''
     image_array = np.array(Image.open(image_path))
     return image_array
+
+def load_normal_image(normal_path: str) -> npt.NDArray[np.float32]:
+    '''
+    Loads a given surface normal image from the disk
+    Inputs:
+    - normal_path: String containing path to the normal file on disk
+    Outputs:
+    - normal_xyz: Numpy array of dimension [H x W x 3], containing the surface normal vector 
+    associated with each pixel w.r.t. the camera frame in order XYZ. 
+    +ve X to right of camera centre
+    +ve Y down from camera centre
+    +ve Z out from camera centre through lens
+    '''
+    normal_rgb = cv2.imread(normal_path, cv2.IMREAD_UNCHANGED)[...,::-1].astype(np.float32)
+    normal_xyz = (normal_rgb - 128) / 128
+    return normal_xyz
